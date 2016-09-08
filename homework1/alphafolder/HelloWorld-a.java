d87e0a5687713b5d9eb972d5ce691ae7de9ae2ec
inDuberitably <duberstein.4@wright.edu>
2016-09-08 15:11:19 -0400

    Removed HelloWorld-a

--- homework1/alphafolder/HelloWorld-a.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-	public static void main(String args[]){
-		//italics
-		System.out.println("\033[We all self-conscious. I'm just the first to admit it.\033[0m - Kanye West");
-}
-}
